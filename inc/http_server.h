bba28a52274eaf6c8753721cbdf8a34958c11dd6
Irefe <nlp_irefe@163.com>
2023-09-10 14:37:05 +0800

    io复用代码

--- inc/http_server.h
+++ inc/http_server.h
@@ -7,14 +7,23 @@ class HttpServer {
 public:
     HttpServer(const std::string ipAddr, const unsigned short int portId, const unsigned int backlog);
     ~HttpServer();
-    bool Init();
+    void Init();
 private:
     bool InitServer();
+    bool InitEpollFd();
+    bool RegisterServerReadEvent();
+    void EventLoop();
+    void HandleServerReadEvent();
+    void HandleClientReadEvent(const int client);
 private:
+    // 服务器socket相关参数
     std::string m_ipAddr;
     unsigned short int m_port;
     int m_server;
     unsigned int m_backlog;
+    // epoll相关参数
+    int m_efd;
+    int m_epollSize;
 };
 
 #endif
--- src/http_server.cpp
+++ src/http_server.cpp
@@ -1,10 +1,14 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/poll.h>
 #include <stdio.h>
 #include <http_server.h>
 
-HttpServer::HttpServer(const std::string ipAddr, const unsigned short int portId,  const unsigned int backlog)
-    : m_ipAddr(ipAddr), m_port(portId), m_server(-1), m_backlog(backlog)
+const unsigned int MAX_READ_BUFF_LEN = 1024; // 每次从客户端最多读取1024字节数据到缓冲区
+
+HttpServer::HttpServer(const std::string ipAddr, const unsigned short int portId,  const unsigned int backlog,
+    const int epollSize)
+    : m_ipAddr(ipAddr), m_port(portId), m_server(-1), m_backlog(backlog), m_efd(-1), m_epollSize(epollSize)
 {}
 
 HttpServer::~HttpServer()
@@ -14,31 +18,40 @@ HttpServer::~HttpServer()
     }
 }
 
-bool HttpServer::Init()
+void HttpServer::Init()
 {
-    if (InitServer == false) {
-        return false;
+    if (InitServer() == false) {
+        return;
     }
 
-    return true;
+    if (InitEpollFd() == false) {
+        return;
+    }
+
+    if (RegisterServerReadEvent() == false) {
+        return;
+    }
+
+    EventLoop();
+
 }
 
 bool HttpServer::InitServer()
 {
     if (m_server != -1) {
-        printf("EVENT  Server alreadly exists.");
+        printf("EVENT  Server alreadly exists.\n");
         return true;
     }
 
     m_server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (m_server == -1) {
-        printf("ERROR  Create socket fail.");
+        printf("ERROR  Create socket fail.\n");
         return false;
     }
 
     in_addr_t ipNum = inet_addr(m_ipAddr.c_str());
     if (ipNum == INADDR_NONE) {
-        printf("ERROR  Invalid ip address.");
+        printf("ERROR  Invalid ip address.\n");
         return false;
     }
 
@@ -49,16 +62,126 @@ bool HttpServer::InitServer()
     if (bind(m_server, reinterpret_cast<struct sockaddr *>(&server_addr), sizeof(server_addr)) == -1) {
         close(m_server);
         m_server = -1;
-        printf("ERROR  server bind fail: %s:%hu", m_ipAddr.c_str(), m_port);
+        printf("ERROR  server bind fail: %s:%hu.\n", m_ipAddr.c_str(), m_port);
         return false;
     }
 
     if (listen(m_server, m_backlog) == -1) {
         close(m_server);
         m_server = -1;
-        printf("ERROR  server listen fail.");
+        printf("ERROR  server listen fail.\n");
         return false;
     }
 
     return true;
+}
+
+bool HttpServer::InitEpollFd()
+{
+    if (m_efd != -1) {
+        return true;
+    }
+
+    m_efd = epoll_create(m_epollSize);
+    if (m_efd == -1) {
+        printf("ERROR  epoll_create fail.\n");
+        return false;
+    }
+
+    return true;
+}
+
+bool HttpServer::RegisterServerReadEvent()
+{
+    struct epoll_event serverEvent = { 0 };
+    serverEvent.events = EPOLLIN;
+    serverEvent.data.fd = m_server;
+    ret = epoll_ctl(m_efd, EPOLL_CTL_ADD, m_server, serverEvent);
+    if (ret == -1) {
+        printf("ERROR  Register server read event fail.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void HttpServer::EventLoop()
+{
+    struct epoll_event *events = new struct epoll_event[m_epollSize];
+    if (events == nullptr) {
+        printf("ERROR  Allooc epoll_event memory fail.\n");
+        return;
+    }
+
+    bool stopFlag = false;
+    while (!stopFlag) {
+        ret = epoll_wait(m_efd, events, m_epollSize, -1);
+        if (ret == -1) {
+            delete[] events;
+            return;
+        }
+        for (unsigned int i = 0; i < static_cast<unsigned int>(ret); ++i) {
+            if (events[i].events & EPOLLIN == 0) {
+                continue;
+            }
+            int socket = events[i].data.fd;
+            if (socket == m_server) {
+                HandleServerReadEvent();
+            } else {
+                HandleClientReadEvent(socket);
+            }
+        }
+    }
+
+    delete []events;
+    return;
+}
+
+void HttpServer::HandleServerReadEvent()
+{
+    struct sockaddr_in clientAddr = { 0 };
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    int client = accept(m_server, reinterpret_cast<struct sockaddr *>(&clientAddr), &clientAddrLen);
+    if (client == -1) {
+        printf("ERROR  accept fail.\n");
+        return;
+    }
+    printf("EVENT  new connect: client[%d] with %s:%hu.\n", client, inet_ntoa(clientAddr.sin_addr.s_addr),
+        ntohs(clientAddr.sin_port));
+
+    // 注册客户端的监听读事件
+    struct epoll_event clientEvent = { 0 };
+    clientEvent.events = EPOLLIN;
+    clientEvent.data.fd = client;
+    ret = epoll_ctl(m_efd, EPOLL_CTL_ADD, client, &clientEvent);
+    if (ret == -1) {
+        printf("ERROR  epoll_ctl fail.\n");
+        close(client);
+        return;
+    }
+}
+
+void HttpServer::HandleClientReadEvent(const int client)
+{
+    char readBuff[MAX_READ_BUFF_LEN + 1] = { 0 };
+    ssize_t n = read(client, readBuff, MAX_READ_BUFF_LEN);
+    if (n <= 0) {
+        ret = epoll_ctl(m_efd, EPOLL_CTL_DEL, client, NULL);
+        if (ret == -1) {
+            return;
+        }
+        close(client);
+    } else {
+        // 调试时，只打印收到的信息，先不回复
+        struct sockaddr_in clientAddr = { 0 };
+        socklen_t clientAddrLen = sizeof(clientAddr);
+        int ret = getsockname(client, reinterpret_cast<struct sockaddr *>(&clientAddr), &clientAddrLen);
+        if (ret == -1) {
+            printf("DEBUG  client recv msg:\n%s\n", readBuff);
+        } else {
+        printf("DEBUG  client %s:%hu recv msg:\n%s\n", inet_ntoa(clientAddr.sin_addr.s_addr),
+            ntohs(clientAddr.sin_port), readBuff);
+        }
+
+    }
 }
