1076958a6eefbc80af0ec23150da61a506d58077
Irefe <nlp_irefe@163.com>
2024-02-08 12:11:28 +0800

    解析头部信息

--- inc/http_processor.h
+++ inc/http_processor.h
@@ -6,13 +6,19 @@ const unsigned int MAX_RECV_BUFF_LEN = 2048;
 enum GetALineStatus : unsigned char {
     GET_A_LINE_OK = 0,
     GET_A_LINE_CONTINUE = 1,
-    GET_A_LINE_WRROR = 2,
+    GET_A_LINE_ERROR = 2,
 };
 
 enum HttpProcessState : unsigned int {
     HTTP_PROCESS_STATE_PARSE_REQUEST_LINE = 0,
     HTTP_PROCESS_STATE_PARSE_HEAD_FIELD = 1,
     HTTP_PROCESS_STATE_PARSE_MESSAGE_BODY = 2,
+};
+
+enum ParseRequestReturnCode: unsigned int {
+    PARSE_REQUEST_RETURN_CODE_FINISH = 0, // 解析请求消息完成
+    PARSE_REQUEST_RETURN_CODE_ERROR = 1, // 解析请求消息出错
+    PARSE_REQUEST_RETURN_CODE_CONTINUE = 2, // 需要继续解析
 };
 
 class HttpProcessor {
@@ -22,7 +28,7 @@ public:
     bool Read();
     bool ProcessRequest();
     GetALineStatus GetALine();
-    bool ParseRequestLine();
+    ParseRequestReturnCode ParseRequestLine();
     bool GetFieldSplitedByWhiteSpaceChars(char *&field);
 
 private:
@@ -36,6 +42,8 @@ private:
     char *m_method{ nullptr };
     char *m_url{ nullptr };
     char *m_httpVersion{ nullptr };
+    unsigned int m_contentLen{ 0 };
+    bool m_keepAlive{ false };
 };
 
 
--- src/http_processor.cpp
+++ src/http_processor.cpp
@@ -5,6 +5,24 @@ const char *WHITE_SPACE_CHARS = " \t";
 const char *GET_METHOD_STR = "GET";
 const char *URL_HTTP_PREFIX = "http://";
 const char URL_SPLIT_CHAR = '/';
+const char END_CHAR = '\0';
+const char *CONTENT_LENGTH_KEY_NAME = "Content-Length";
+const char *CONNECTION_KEY_NAME = "Connection";
+const char *KEEP_ALIVE_VALUE = "keep-alive";
+
+void (HttpProcessor::*ParseHeadFieldValueStr)();
+typedef struct {
+    const char *keyName;
+    ParseHeadFieldValueStr parseFunc;
+} KeyNameAndParseFuncMap;
+
+const KeyNameAndParseFuncMap KEY_NAME_AND_PARSE_FUNC_MAP_LIST[] = {
+    { CONTENT_LENGTH_KEY_NAME, HttpProcessor::ParseContentLength },
+    { CONNECTION_KEY_NAME, HttpProcessor::ParseConnection },
+};
+
+const KeyNameAndParseFuncMap KEY_NAME_AND_PARSE_FUNC_MAP_LIST_SIZE =
+    sizeof(KEY_NAME_AND_PARSE_FUNC_MAP_LIST) / sizeof(KEY_NAME_AND_PARSE_FUNC_MAP_LIST[0]);
 
 HttpProcessor::HttpProcessor(socketId) : m_socketId(socketId)
 {}
@@ -62,20 +80,20 @@ GetALineStatus HttpProcessor::GetALine()
     return GET_A_LINE_CONTINUE;
 }
 
-bool HttpProcessor::ParseRequestLine()
+ParseRequestReturnCode HttpProcessor::ParseRequestLine()
 {
     if (GetFieldSplitedByWhiteSpaceChars(m_method) == false) {
         printf("ERROR Get method fail.\n");
-        return false;
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
     }
     if (strcasecmp(m_httpVersion, GET_METHOD_STR) != 0) {
         printf("ERROR Support GET method only.\n");
-        return false;
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
     }
 
     if (GetFieldSplitedByWhiteSpaceChars(m_url) == false) {
         printf("ERROR Get url fail.\n");
-        return false;
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
     }
     printf("EVENT Origin url: %s.\n", m_url);
     if (strncasecmp(m_url, URL_HTTP_PREFIX, strlen(URL_HTTP_PREFIX)) == 0) {
@@ -84,15 +102,15 @@ bool HttpProcessor::ParseRequestLine()
     }
     if (m_url == nullptr || m_url[0] != URL_SPLIT_CHAR) {
         printf("ERROR Invalid url.\n");
-        return false;
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
     }
     
     m_httpVersion = m_startIndex;
     if (strcasecmp(m_httpVersion, "HTTP/1.0") != 0) {
         printf("ERROR Support HTTP/1.0 only.\n");
-        return false;
+        return PARSE_REQUEST_RETURN_CODE_ERROR;
     }
-    return true;
+    return PARSE_REQUEST_RETURN_CODE_CONTINUE;
 }
 
 bool HttpProcessor::GetFieldSplitedByWhiteSpaceChars(char *&field)
@@ -107,4 +125,38 @@ bool HttpProcessor::GetFieldSplitedByWhiteSpaceChars(char *&field)
     ret++;
     m_startIndex = ret + strspn(ret, whiteSpaceChars);
     return true;
+}
+
+ParseRequestReturnCode HttpProcessor::ParseHeadFields()
+{
+    if (m_startIndex == END_CHAR) {
+        if (m_contentLen != 0) {
+            m_processState = HTTP_PROCESS_STATE_PARSE_MESSAGE_BODY;
+            return ARSE_REQUEST_RETURN_CODE_CONTINUE;
+        }
+        return PARSE_REQUEST_RETURN_CODE_FINISH;
+    }
+
+    for (unsigned int i = 0; i < KEY_NAME_AND_PARSE_FUNC_MAP_LIST_SIZE; ++i) {
+        KeyNameAndParseFuncMap map = KEY_NAME_AND_PARSE_FUNC_MAP_LIST[i];
+        if (strncasecmp(m_startIndex, map.keyName, strlen(map.keyName)) == 0) {
+            m_startIndex += strlen(map.keyName);
+            m_startIndex += 1; // 跳过':'
+            m_startIndex += strspn(m_startIndex, "\t ");
+            map.parseFunc();
+        }
+    }
+    return PARSE_REQUEST_RETURN_CODE_CONTINUE;
+}
+
+void HttpProcessor::ParseContentLength()
+{
+    m_contentLen = atol(m_startIndex);
+}
+
+void HttpProcessor::ParseConnection()
+{
+    if (strcasecmp(m_startIndex, KEEP_ALIVE_VALUE) == 0) {
+        m_keepAlive = true;
+    }
 }
